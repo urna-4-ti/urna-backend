ac270f91b4e453d0bfd649cf043e6201b676b948
Rafael Vargas <rafael.vargas.emp@gmail.com>
2024-04-04 13:16:37 -0300

    [WIP] add findAll for political party

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -12,4 +12,23 @@ type UserJWTPayload = {
 	name: string;
 };
 
-export type { UserJWTPayload };
+enum classes2 {
+	ti1 = "TI_1",
+	TI_2 = "TI_2",
+	TI_3 = "TI_3",
+	TI_4 = "TI_4",
+	TQ_1 = "TQ_1",
+	TQ_2 = "TQ_2",
+	TQ_3 = "TQ_3",
+	TQ_4 = "TQ_4",
+	TMA_1 = "TMA_1",
+	TMA_2 = "TMA_2",
+	TMA_3 = "TMA_3",
+	TMA_4 = "TMA_4",
+	TA_1 = "TA_1",
+	TA_2 = "TA_2",
+	TA_3 = "TA_3",
+	TA_4 = "TA_4",
+}
+
+export { type UserJWTPayload, classes2 };
