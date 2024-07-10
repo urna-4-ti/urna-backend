45239ffded49ec315d87f7e939ffb1693b87edef
devvieiira <kauavieira900@gmail.com>
2024-07-10 16:47:14 -0300

    [WIP] att

--- src/http/routes/candidate/findAll.ts
+++ src/http/routes/candidate/findAll.ts
@@ -107,3 +107,65 @@ export async function FindCandidatesId(app: FastifyInstance) {
 		}
 	});
 }
+
+interface ClassParams {
+	class: string;
+}
+
+export async function FindCandidatesClass(app: FastifyInstance) {
+	app.get<{ Params: ClassParams }>(
+		"/candidate/filter/:class",
+		async (req, reply) => {
+			const { class: CandidateClass } = req.params;
+			let userJWTData: UserJWTPayload | null = null;
+			try {
+				const authorization = req.headers.authorization;
+				const access_token = authorization?.split("Bearer ")[1];
+				userJWTData = app.jwt.decode(access_token as string);
+			} catch (error) {
+				return reply.status(403).send({
+					error: error,
+					message: "Token Missing",
+				});
+			}
+
+			const loggedUser = await prisma.user.findUnique({
+				where: {
+					email: userJWTData?.email,
+				},
+			});
+
+			if (loggedUser?.role !== "ADMIN") {
+				return reply.status(403).send({
+					message: "Action not permitted",
+				});
+			}
+
+			try {
+				const candidate = await prisma.candidate.findMany({
+					where: {
+						PoliticalParty: {
+							class: CandidateClass,
+						},
+					},
+					include: {
+						PoliticalParty: {
+							select: {
+								class: true,
+							},
+						},
+					},
+				});
+				return reply.status(201).send({
+					data: candidate,
+				});
+				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			} catch (err: any) {
+				return reply.status(403).send({
+					message: err.message,
+					statusCode: 403,
+				});
+			}
+		},
+	);
+}
